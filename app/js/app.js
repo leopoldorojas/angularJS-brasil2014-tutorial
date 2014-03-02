00e5d46408e80bf14d97570becf7016dc64e67fd
Leopoldo Rojas Moguel <leopoldo.rojas@gmail.com>
2014-03-01 21:50:22 -0600

    paso-3

--- app/js/app.js
+++ app/js/app.js
@@ -11,6 +11,40 @@ angular.module('myApp', [
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/teams', {templateUrl: 'partials/team-list.html', controller: 'TeamListCtrl'});
-  $routeProvider.when('/team-detail', {templateUrl: 'partials/team-detail.html', controller: 'TeamDetailCtrl'});
+  $routeProvider.when('/teams/:teamId', {templateUrl: 'partials/team-detail.html', controller: 'TeamDetailCtrl'});
   $routeProvider.otherwise({redirectTo: '/teams'});
-}]);
+}]).
+factory('Teams', function(){
+	return {
+		'get': 
+			[
+
+			    {
+			        "id": "costa_rica", 
+			        "imageUrl": "img/teams/costa-rica.0.jpg", 
+			        "name": "Costa Rica",
+			        "group": "D", 
+			        "headline": "Costa Rica ganó todos los partidos que disputó en su casa en la ronda final.",
+			        "chances": 0.10,
+			        "confederation": "Concacaf",
+			        "championships": 0,
+			        "worldCups": 4,
+			        "rankingFifa": 35
+			    },
+
+			    {
+			        "id": "uruguay", 
+			        "imageUrl": "img/teams/uruguay.0.jpg", 
+			        "name": "Uruguay",
+			        "group": "D", 
+			        "headline": "Tras superar con solvencia el partido de ida en Jordania por 5-0, Uruguay cerró con un 0-0 ante su público esta instancia para sellar su boleto a Brasil.",
+			        "chances": 0.09,
+			        "confederation": "Conmebol",
+			        "championships": 2,
+			        "worldCups": 11,
+			        "rankingFifa": 7
+			    }  
+
+		  	]
+	}
+});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,38 +3,16 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('TeamListCtrl', ['$scope', function($scope) {
+  controller('TeamListCtrl', ['$scope', 'Teams', function($scope, Teams) {
   	$scope.orderProp="name";
-  	$scope.teams = [
-
-	    {
-	        "id": "costa_rica", 
-	        "imageUrl": "img/teams/costa-rica.0.jpg", 
-	        "name": "Costa Rica",
-	        "group": "D", 
-	        "headline": "Costa Rica ganó todos los partidos que disputó en su casa en la ronda final.",
-	        "chances": 0.10,
-	        "confederation": "Concacaf",
-	        "championships": 0,
-	        "worldCups": 4,
-	        "rankingFifa": 35
-	    },
-
-	    {
-	        "id": "uruguay", 
-	        "imageUrl": "img/teams/uruguay.0.jpg", 
-	        "name": "Uruguay",
-	        "group": "D", 
-	        "headline": "Tras superar con solvencia el partido de ida en Jordania por 5-0, Uruguay cerró con un 0-0 ante su público esta instancia para sellar su boleto a Brasil.",
-	        "chances": 0.09,
-	        "confederation": "Conmebol",
-	        "championships": 2,
-	        "worldCups": 11,
-	        "rankingFifa": 7
-	    }  
-
-  	];
+  	$scope.teams = Teams.get;
   }])
-  .controller('TeamDetailCtrl', [function() {
+  .controller('TeamDetailCtrl', ['$scope', '$routeParams', 'Teams', function($scope, $routeParams, Teams) {
+
+    var teams = Teams.get;
+
+  	$scope.team = teams.filter(function(team) {
+  		return team.id == $routeParams.teamId
+  	})[0];
 
   }]);
